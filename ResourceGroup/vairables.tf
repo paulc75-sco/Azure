4afcead13e608c05ba8f3fb0e6bb86dc428b4b3a
Paul CHeyne <paulc75@hotmail.com>
2024-10-27 20:49:34 +0000

    Add Resource Group Code

---
+++ ResourceGroup/vairables.tf
@@ -0,0 +1,23 @@
+variable "name" {
+
+}
+
+variable "location" {
+  default = "West Europe"
+
+}
+
+variable "environment" {
+  default = "Missing Tag"
+
+}
+
+variable "group" {
+  default = "Digital Transformation"
+
+}
+
+variable "owner" {
+  default = "Digital Transformation"
+
+}
